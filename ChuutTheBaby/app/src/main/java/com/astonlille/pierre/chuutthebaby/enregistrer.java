4bd58dcaa27fcccf79ea7aa1952bf15337dda570
revoLille <pierre.florquin@gmail.com>
2016-05-19 22:53:59 +0200

    classe enregistrer
    une classe en plus

---
+++ ChuutTheBaby/app/src/main/java/com/astonlille/pierre/chuutthebaby/enregistrer.java
@@ -0,0 +1,9 @@
+package com.astonlille.pierre.chuutthebaby;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Pierre on 19/05/2016.
+ */
+public class enregistrer extends Fragment{
+}
