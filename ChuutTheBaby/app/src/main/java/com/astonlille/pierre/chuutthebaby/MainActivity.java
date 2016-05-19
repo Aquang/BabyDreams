b16674e916bdeb5f8fd762dad9e69c0f9b6a58cc
revoLille <pierre.florquin@gmail.com>
2016-05-19 22:53:23 +0200

    third day
    troisième jour : la méthode onNavigationItemSelected ne fonctionne pas ,
    sûrement à  cause du grand vide dans ma méthode enregistrer

--- ChuutTheBaby/app/src/main/java/com/astonlille/pierre/chuutthebaby/MainActivity.java
+++ ChuutTheBaby/app/src/main/java/com/astonlille/pierre/chuutthebaby/MainActivity.java
@@ -4,6 +4,8 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -82,11 +84,15 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment;
 
         if (id == R.id.nav_play) {
-            // Handle the playing action
-        } else if (id == R.id.nav_rec) {
 
+        } else if (id == R.id.nav_rec) {
+            fragment = new enregistrer();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.enregistrer, fragment);
+            ft.commit();
         } else if (id == R.id.nav_animation) {
 
         } else if (id == R.id.nav_auto) {
